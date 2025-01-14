bb7577b4e3b5d236fa7607d77e21ecabe33ff4af
EC0GAME <65595888+EC0GAME@users.noreply.github.com>
2025-01-14 13:21:58 -0500

    Added JWT generation
    In routes.go, added new package, secret variable, and the function for handling JWT generation

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -12,8 +12,13 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 
+	"github.com/golang-jwt/jwt/v4"
+
 	"github.com/coder/websocket"
 )
+
+// replace here
+var jwtSecret = []byte("shhhhhhh")
 
 func (s *Server) RegisterRoutes() http.Handler {
 	r := chi.NewRouter()
@@ -32,6 +37,8 @@ func (s *Server) RegisterRoutes() http.Handler {
 	r.Get("/health", s.healthHandler)
 
 	r.Get("/websocket", s.websocketHandler)
+	
+	r.Post("/generateJWT", s.generateJWTHandler)
 
 	return r
 }
@@ -77,3 +84,32 @@ func (s *Server) websocketHandler(w http.ResponseWriter, r *http.Request) {
 		time.Sleep(time.Second * 2)
 	}
 }
+
+func (s *Server) generateJWTHandler(w http.ResponseWriter, r *http.Request) {
+	var req struct {
+		Email string `json:"email"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	// claims for documentation
+	claims := &jwt.StandardClaims{
+		Subject:   req.Email,
+		ExpiresAt: time.Now().Add(168 * time.Hour).Unix(),
+	}
+
+	// token creation
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString(jwtSecret)
+	if err != nil {
+		http.Error(w, "Failed to generate token", http.StatusInternalServerError)
+		return
+	}
+
+	// return with header
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"token": signedToken})
+}
+
