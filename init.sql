5bdbabf5fe9ee5c099e2802b21fa11914a5fa29a
bingKegeta <sohamg24@gmail.com>
2025-01-24 00:25:32 -0500

    Trim unrequired elements of the db

--- init.sql
+++ init.sql
@@ -3,18 +3,9 @@ CREATE TABLE IF NOT EXISTS Users (
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    oauth_provider VARCHAR(50),
-    oauth_provider_id VARCHAR(255),
-    oauth_token VARCHAR(255),
-    oauth_token_expires_at TIMESTAMP,
     auth_token VARCHAR(255),
     auth_token_created_at TIMESTAMP,
-    CONSTRAINT chk_password_or_oauth CHECK (
-            (password IS NOT NULL AND oauth_provider IS NULL AND oauth_provider_id IS NULL) OR
-            (password IS NULL AND oauth_provider IS NOT NULL AND oauth_provider_id IS NOT NULL)
-        )
 );
 
 CREATE INDEX IF NOT EXISTS idx_users_email ON Users(email);
-CREATE INDEX IF NOT EXISTS idx_users_oauth_provider_id ON Users(oauth_provider, oauth_provider_id);
 CREATE INDEX IF NOT EXISTS idx_users_auth_token ON Users(auth_token);
