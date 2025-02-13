71d6719415c5d2ef137137522bca7ebbfd78cbca
Brian Conn <156158220+BrianConn2025@users.noreply.github.com>
2025-02-13 01:20:13 -0500

    Update init.sql with table for user metrics

--- init.sql
+++ init.sql
@@ -9,3 +9,17 @@ CREATE TABLE IF NOT EXISTS Users (
 
 CREATE INDEX IF NOT EXISTS idx_users_email ON Users(email);
 CREATE INDEX IF NOT EXISTS idx_users_auth_token ON Users(auth_token);
+
+-- WebSocketMessages table to store incoming messages
+CREATE TABLE websocket_messages (
+    id SERIAL PRIMARY KEY,
+    x FLOAT8,
+    y FLOAT8,
+    second FLOAT8,
+    is_video BOOLEAN,
+    video_duration FLOAT8
+);
+
+
+-- Index on user_id for faster lookups of messages by user
+CREATE INDEX IF NOT EXISTS idx_websocket_messages_user_id ON WebSocketMessages(user_id);
