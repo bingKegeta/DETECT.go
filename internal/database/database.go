0cab221b50b6b8cd1d897155d740d2442f290d1f
EC0GAME <65595888+EC0GAME@users.noreply.github.com>
2025-03-09 13:09:58 -0400

    Update database.go

--- internal/database/database.go
+++ internal/database/database.go
@@ -11,7 +11,6 @@ import (
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 	_ "github.com/joho/godotenv/autoload"
-	_ "github.com/lib/pq" 
 )
 
 // Service represents a service that interacts with a database.
