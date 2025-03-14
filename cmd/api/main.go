8db126d43e765d4887ca178da5a99946816740c8
EC0GAME <65595888+EC0GAME@users.noreply.github.com>
2025-03-14 02:43:46 -0400

    bug testing

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -39,7 +39,7 @@ func gracefulShutdown(apiServer *http.Server, done chan bool) {
 
 func main() {
 	auth.NewAuth()
-	apiServer := server.NewServer()
+	apiServer := apiServer.NewServer()
 
 	// Create a done channel to signal when the shutdown is complete
 	done := make(chan bool, 1)
